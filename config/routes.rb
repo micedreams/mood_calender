d2786c389afd9f76c15520cc488bb30ee74fa3ac
Akshatha <micedreams@gmail.com>
2019-03-09 15:46:28 +0530

    mostly done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :feels, except: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/weekly_view" => "feels#weekly_view", as: :weekly_view
 end
